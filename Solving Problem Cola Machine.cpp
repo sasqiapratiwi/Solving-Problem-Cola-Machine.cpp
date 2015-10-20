80a7b25c71862f3d86b234dde2a5ec45a3fcd640
sasqiapratiwi <sasqiaphat@gmail.com>
2015-10-20 23:41:15 +0700

    Create Solving Problem Cola Machine.cpp

---
+++ Solving Problem Cola Machine.cpp
@@ -0,0 +1,52 @@
+/*
+ * Hello.cpp
+ *
+ *  Created on: 19 Okt 2015
+ *      Author: Sasqia Pratiwi Hanum AT | 1517051141
+ */
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int a,b;
+	cout<<"----- SASQIA PRATIWI HANUM -----"<<endl<<endl;
+	cout<<"1.Mesin Minuman"<<endl;
+	cout<<"2.Keluar Mesin"<<endl;
+	cout<<"--------------------------------"<<endl;
+	cout<<"Pilih Angka Menu :"; cin>>a;
+	
+	switch (a)
+	{
+	case 1: cout<<"Nama Minuman :"<<endl ;	
+	cout<<"1. Coca Cola"<<endl;
+	cout<<"2. Fanta"<<endl;
+	cout<<"3. Sprite"<<endl;
+	cout<<"4. Pepsi"<<endl;
+	cout<<"5. Pulpy"<<endl;
+	
+	if (cout<<"Masukkan Pilihan: ") cin>>b;
+	{
+	if (b==1)
+	cout<<"Anda dapat Coca Cola";
+	else if (b==2)
+	cout<<"Anda dapat Fanta";
+	else if (b==3)
+	cout<<"Anda dapat Sprite";
+	else if (b==4)
+	cout<<"Anda dapat Pepsi";
+	else if (b==5)
+	cout<<"Anda dapat Pulpy";
+	else if ((b>=6)&&(b<=1000))
+	cout<<"Error.\nchoice was not valid, here is your money back.";
+	break;
+	}
+	
+	case 2: cout<<"EXIT PROGRAM!";
+	break;
+	default:
+	cout<<"Error.\nchoice was not valid, here is your money back.";	
+}
+return 0;
+}
